22c2e7ab2f6bd7044fc1718a501ed02f5ff80f45
Vassilis Choutas <vassilis.choutas@tuebingen.mpg.de>
2019-11-22 12:08:01 +0100

    Make igl run optional and support loading existing timings in benchmark_time

--- examples/benchmark_time.py
+++ examples/benchmark_time.py
@@ -21,6 +21,7 @@ from __future__ import division
 
 import sys
 import os
+import os.path as osp
 
 import time
 
@@ -35,7 +36,7 @@ import open3d as o3d
 import pyigl as igl
 from iglhelpers import p2e, e2p
 
-import tqdm
+import yaml
 
 import torch
 import torch.nn as nn
@@ -60,17 +61,26 @@ if __name__ == "__main__":
     parser.add_argument('--mesh-fn', type=str, dest='mesh_fn',
                         help='A mesh file (.obj, .ply, e.t.c.) to be checked' +
                         ' for collisions')
+    parser.add_argument('--timings-fn', type=str, dest='timings_fn',
+                        default='timings.yaml',
+                        help='File where the timings will be saved')
     parser.add_argument('--num-query-points', type=int, default=1,
-                        dest='num_query_points',
+                        nargs='+',
+                        dest='num_query_points_lst',
                         help='Number of random query points')
     parser.add_argument('--timing-iters', type=int, default=1000,
                         dest='timing_iters')
+    parser.add_argument('--run-igl', dest='run_igl',
+                        type=lambda arg: arg.lower() in ['true'],
+                        default=False)
 
     args, _ = parser.parse_known_args()
 
     mesh_fn = args.mesh_fn
-    num_query_points = args.num_query_points
+    num_query_points_lst = args.num_query_points_lst
     timing_iters = args.timing_iters
+    timings_fn = args.timings_fn
+    run_igl = args.run_igl
 
     input_mesh = Mesh(filename=mesh_fn)
 
@@ -86,48 +96,62 @@ if __name__ == "__main__":
                          dtype=torch.long,
                          device=device)
 
-    query_points = torch.rand([1, num_query_points, 3], dtype=torch.float32,
-                              device=device) * 2 - 1
-    query_points_np = query_points.detach().cpu().numpy().squeeze(
-        axis=0).astype(np.float32).reshape(num_query_points, 3)
+    if osp.exists(timings_fn):
+        with open(timings_fn, 'r') as f:
+            output_dict = yaml.load(f)
+    else:
+        output_dict = {}
 
-    batch_size = 1
-    triangles = vertices[faces].unsqueeze(dim=0)
+    for num_query_points in num_query_points_lst:
+        query_points = torch.rand([1, num_query_points, 3], dtype=torch.float32,
+                                  device=device) * 2 - 1
+        query_points_np = query_points.detach().cpu().numpy().squeeze(
+            axis=0).astype(np.float32).reshape(num_query_points, 3)
 
-    m = bvh_distance_queries.BVH()
-    elapsed = 0
+        batch_size = 1
+        triangles = vertices[faces].unsqueeze(dim=0)
 
-    for n in tqdm.tqdm(range(timing_iters)):
-        torch.cuda.synchronize()
-        start = time.perf_counter()
-        distances, closest_points, closest_faces = m(triangles, query_points)
-        torch.cuda.synchronize()
-        elapsed += (time.perf_counter() - start)
+        m = bvh_distance_queries.BVH()
 
-    logger.info(f'CUDA Elapsed time {elapsed / timing_iters}')
-    #  print(outputs[2])
-    distances = distances.detach().cpu().numpy()
-    closest_points = closest_points.detach().cpu().numpy().squeeze()
+        elapsed = 0
+        for n in tqdm.tqdm(range(timing_iters)):
+            torch.cuda.synchronize()
+            torch.cuda.synchronize()
+            start = time.perf_counter()
+            distances, closest_points, closest_faces = m(
+                triangles, query_points)
+            torch.cuda.synchronize()
+            elapsed += (time.perf_counter() - start)
 
-    #  logger.info(f'Distances: {distances ** 2}')
-    #  logger.info(f'Closest points: {closest_points}')
+        cuda_elapsed = elapsed / timing_iters
+        logger.info(
+            f'CUDA Points = {num_query_points}: elapsed time {cuda_elapsed}')
+        distances = distances.detach().cpu().numpy()
+        closest_points = closest_points.detach().cpu().numpy().squeeze()
+        output_dict[num_query_points]['cuda'] = cuda_elapsed
 
-    #  outputs = outputs.detach().cpu().numpy().squeeze()
+        if run_igl:
+            elapsed = 0
+            for n in tqdm.tqdm(range(timing_iters)):
+                sqrD = igl.eigen.MatrixXd()
+                closest_faces = igl.eigen.MatrixXi()
+                closest_points_eig = igl.eigen.MatrixXd()
+                query_points_eigen = p2e(query_points_np)
+                v_eig = p2e(v)
+                f_eig = p2e(input_mesh.f.astype(np.int64))
 
-    sqrD = igl.eigen.MatrixXd()
-    closest_faces = igl.eigen.MatrixXi()
-    closest_points_eig = igl.eigen.MatrixXd()
+                start = time.perf_counter()
+                #  Find the closest points on the SMPL-X mesh
+                igl.point_mesh_squared_distance(
+                    query_points_eigen,
+                    v_eig, f_eig,
+                    sqrD, closest_faces, closest_points_eig)
+                elapsed += (time.perf_counter() - start)
 
-    query_points_eigen = p2e(query_points_np)
+            igl_elapsed = elapsed / timing_iters
+            logger.info(
+                f'LibIGL Points = {num_query_points}: elapsed time {igl_elapsed}')
+            output_dict[num_query_points]['igl'] = igl_elapsed
 
-    elapsed = 0
-
-    for n in tqdm.tqdm(range(timing_iters)):
-        start = time.perf_counter()
-        # Find the closest points on the SMPL-X mesh
-        igl.point_mesh_squared_distance(query_points_eigen,
-                                        p2e(v),
-                                        p2e(input_mesh.f.astype(np.int64)),
-                                        sqrD, closest_faces, closest_points_eig)
-        elapsed += (time.perf_counter() - start)
-    logger.info(f'LibIGL Elapsed time {elapsed / timing_iters}')
+    with open(timings_fn, 'w') as f:
+        yaml.dump(output_dict, f)
