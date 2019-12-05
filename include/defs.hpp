246d8ad05152215e732dfd331b4efe8be32ac975
Vassilis Choutas <vassilis.choutas@tuebingen.mpg.de>
2019-12-05 15:50:04 +0100

    Create template versions for make_vec3

--- include/defs.hpp
+++ include/defs.hpp
@@ -13,4 +13,37 @@ template <typename T>
 using vec2 = typename std::conditional<std::is_same<T, float>::value, float2,
                                        double2>::type;
 
+float3 make_float3(double3 vec) {
+    return make_float3(vec.x, vec.y, vec.z);
+}
+
+float3 make_float3(double x, double y, double z) {
+    return make_float3(x, y, z);
+}
+
+double3 make_double3(float3 vec) {
+    return make_double3(vec.x, vec.y, vec.z);
+}
+
+double3 make_double3(float x, float y, float z) {
+    return make_double3(x, y, z);
+}
+
+template <typename T>
+__host__ __device__
+vec3<T> make_vec3(T x, T y, T z) {
+}
+
+template <>
+__host__ __device__
+vec3<float> make_vec3(float x, float y, float z) {
+    return make_float3(static_cast<float>(x), static_cast<float>(y), static_cast<float>(z));
+}
+
+template <>
+__host__ __device__
+vec3<double> make_vec3(double x, double y, double z) {
+    return make_double3(static_cast<double>(x), static_cast<double>(y), static_cast<double>(z));
+}
+
 #endif // ifndef DEFINITIONS_H
